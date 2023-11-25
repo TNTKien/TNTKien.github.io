b0a61081a515918d3a27d501e0380e194ab0aa35
iamneyk <iamneyk@gmail.com>
2023-11-26 06:32:42 +0700

    search

--- js/main.js
+++ js/main.js
@@ -34,8 +34,50 @@
 
     // Search model
     $('.search-switch').on('click', function () {
-        // $('.search-model').fadeIn(400);
-        alert("Để cho đẹp thôi chứ chả tác dụng gì đâu 🐧")
+        $('.search-model').fadeIn(400);
+        // alert("Để cho đẹp thôi chứ chả tác dụng gì đâu 🐧")
+    });
+
+    $('.search-model-form').on('submit', async function () {
+        const input = $('#search-input').val();
+        if(!input) {
+            $('.search-model').fadeOut(400, function () {
+                $('#search-input').val('');
+            });
+            return;
+        }
+
+        const api = "https://bltx-backend-677c381e9aae.herokuapp.com/api/search?key=" + input;
+        const response = await fetch(api);
+        const data = await response.json();
+        if (data.length == 0) {
+            $('#hometitle').html(`<h4>Kết quả cho: "${input}"</h4>`);
+            $('#homepage').html(`<h5 style="padding-left:15px">Không tìm thấy truyện nào!</h5>`);
+            $('.search-model').fadeOut(400, function () {});
+            return;
+        }
+        let html = "";
+        data.forEach((element) => {
+            html += `
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="product__item">
+                <a href="${element.url}">
+                    <div class="product__item__pic set-bg" data-setbg="${element.cover}" style="background-image: url(&quot;${element.cover}&quot;);">
+                        <div class="ep">${element.status}</div>
+                    </div>
+                </a>
+                <div class="product__item__text">
+                    <h5><a href="${element.url}">${element.title}</a>
+                    </h5>
+                </div>
+                </div>
+            </div>
+        `;
+        });
+        $('#hometitle').html(`<h4>Kết quả cho: "${input}"</h4>`);
+        $('#homepage').html(html);
+        $('.search-model').fadeOut(400, function () {});
+        
     });
 
     $('.icon_profile').on('click', function () {
